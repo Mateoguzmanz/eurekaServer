f2185884831327ca8c0c59c9adc3c5a6d917403d
mateguzm <mateoguzmanz@hotmail.com>
2023-12-15 19:50:19 -0500

    TODO

---
+++ src/test/java/com/example/Prestamoseurekaserver/PrestamosEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Prestamoseurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrestamosEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
